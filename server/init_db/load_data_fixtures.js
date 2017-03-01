279fbe457073eec6f82fc01c55ce22faa869794c
sur5ive <karpo.korotko@gmail.com>
2017-03-01 18:03:01 +0200

    DB sorting function to lib folder

--- server/init_db/load_data_fixtures.js
+++ server/init_db/load_data_fixtures.js
@@ -5171,66 +5171,6 @@ Meteor.startup(function () {
 
 	];
 
-	/*
-	if (Goals.find().count() < 1){
-		goals.forEach(function(goal){
-			Goals.insert({
-				id:			goal.id,
-				fixture_id:	goal.fixture_id,
-				player_id: 	goal.player_id,
-				min: 		goal.min,
-				cl_goal: 	goal.cl_goal
-			});
-		}); // end of foreach
-		
-		console.log("Startup goals: " + Goals.find().count());
-	
-	} // end of if
-
-	if (Cards.find().count() < 1){
-		cards.forEach(function(card){
-			Cards.insert({
-				id:			card.id,
-				fixture_id:	card.fixture_id,
-				player_id: 	card.player_id,
-				min: 		card.min,
-				cl_card: 	card.cl_card
-			});
-		}); // end of foreach
-		
-		console.log("Startup cards: " + Cards.find().count());
-	
-	} // end of if
-
-	if (Appearances.find().count() < 1){
-		appearances.forEach(function(appearance){
-			Appearances.insert({
-				id:			appearance.id,
-				fixture_id:	appearance.fixture_id,
-				player_id: 	appearance.player_id,
-				in: 		appearance.in,
-				out: 		appearance.out
-			});
-		}); // end of foreach
-		
-		console.log("Startup appearances: " + Appearances.find().count());
-	
-	} // end of if
-*/
-	// Custom function to gather all stats (e.g. goals, 
-	// cards, appearances) into separate arrays, grouped by the fixture id
-	Array.prototype.groupBy = function(prop) {  		// define a new Array function to take the name of the 
-														// grouping property ("fixture_id") as the parameter; 
-  		return this.reduce(function(stats, item) {		// use Array.reduce() for accumulating the stats values; 
-    		var val = item[prop];						// value corresponding to property "fixture_id" fo current stats item
-    		stats[val] = stats[val] || [];				// if stats item with "fixture_id" value exists then use it, 
-														// otherwise assign a new empty array 
-    		stats[val].push(item);						// push the current stats item into the grouped array under 
-														// the respective "fixture_id" value;
-    		return stats;								// return the new stats array, grouped by "fixture_id"
-  		}, {});
-	}
-
 	// Group the stats arrays by "fixture_id" value
 	goals = goals.groupBy("fixture_id");			 
 	cards = cards.groupBy("fixture_id");
